f138a170fe91822dc5711350a58696e09342e26f
Yao Fu <fuyao3860@gmail.com>
2023-03-08 20:40:09 +0000

    add build

---
+++ build.sh
@@ -0,0 +1,8 @@
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/install \
+-DTRITON_BACKEND_REPO_TAG=r22.08 \
+-DTRITON_CORE_REPO_TAG=r22.08 \
+-DTRITON_COMMON_REPO_TAG=r22.08 \
+-DTRITON_PYTORCH_DOCKER_IMAGE="nvcr.io/nvidia/pytorch:22.08-py3" ..
+cmake --build . --target install -j$(nproc)
